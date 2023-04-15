c72dac05ac4a0acfa53bbb16c3e81e7d844fb2d8
ersan <ersan@MSI>
2023-04-15 08:28:12 +0200

    Add project files.

---
+++ EntityLayer/Concrate/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Category
+    {
+        public int CatogeryID { get; set; }
+        public string CatogeryName { get; set; }
+        public string CategoryDesription { get; set; }
+        public bool CategoryStatus { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+
+    }
+}
